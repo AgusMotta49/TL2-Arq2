022695ace9dff34200d2a264cda7ed56152e0409
Danilo Medina <danilomedina666@gmail.com>
2025-05-02 18:01:36 -0300

    delete firebase-config.js

--- WEBCloud/public/firebase-config.js
+++
@@ -1,7 +0,0 @@
-// firebase-config.js
-const firebaseConfig = {
-    apiKey: "AIzaSyBUF-oj6rDEzZFlttKXjzUBY4OaVQYNtLE",
-    databaseURL: "https://arq2-tl2-temperatura-humedad-default-rtdb.firebaseio.com",
-    projectId: "arq2-tl2-temperatura-humedad"
-};
-export { firebaseConfig };
